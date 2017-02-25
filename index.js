ac8290cabf489b01d415521d9471f334bcf591e5
fiatjaf <fiatjaf@gmail.com>
2017-02-25 13:05:47 -0500

    remove commas and fix spacing.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var React = require('react');
+var React = require('react')
 
 var TangleText = React.createClass({
   propTypes: {
@@ -23,92 +23,91 @@ var TangleText = React.createClass({
       onInput: function() { }
     };
   },
-  componentWillMount: function() {
-    this.__isMouseDown = false;
+  componentWillMount: function () {
+    this.__isMouseDown = false
   },
-  componentWillReceiveProps: function(nextProps) {
-    this.setState({ value: nextProps.value });
+  componentWillReceiveProps: function (nextProps) {
+    this.setState({ value: nextProps.value })
   },
-  getInitialState: function() {
-    return { value: this.props.value };
+  getInitialState: function () {
+    return { value: this.props.value }
   },
-  bounds: function(num) {
-    num = Math.max(num, this.props.min);
-    num = Math.min(num, this.props.max);
-    return num;
+  bounds: function (num) {
+    num = Math.max(num, this.props.min)
+    num = Math.min(num, this.props.max)
+    return num
   },
-  onChange: function(e) {
-    this.setState({ value: e.target.value });
+  onChange: function (e) {
+    this.setState({ value: e.target.value })
   },
-  onBlur: function(e) {
-    var parsed = parseFloat(this.state.value);
+  onBlur: function (e) {
+    var parsed = parseFloat(this.state.value)
     if (isNaN(parsed)) {
-      this.setState({ value: this.props.value });
+      this.setState({ value: this.props.value })
     } else {
-      this.props.onChange(this.bounds(parsed));
-      this.setState({ value: this.bounds(parsed) });
+      this.props.onChange(this.bounds(parsed))
+      this.setState({ value: this.bounds(parsed) })
     }
   },
-  onMouseMove: function(e) {
-    var change;
+  onMouseMove: function (e) {
+    var change
     if (this.props.pixelDistance > 0) {
-      change = Math.floor((this.startX - e.screenX) / this.props.pixelDistance);
+      change = Math.floor((this.startX - e.screenX) / this.props.pixelDistance)
     } else {
-      change = this.startX - e.screenX;
+      change = this.startX - e.screenX
     }
-    this.dragged = true;
-    var value = this.bounds(this.startValue - (change * this.props.step));
-    this.setState({ value: value });
-    this.props.onInput(value);
+    this.dragged = true
+    var value = this.bounds(this.startValue - (change * this.props.step))
+    this.setState({ value: value })
+    this.props.onInput(value)
   },
-  onMouseDown: function(e) {
+  onMouseDown: function (e) {
     // short circuit if currently editing number
-    if (e.target === document.activeElement || e.button !== 0) return;
-    this.__isMouseDown = true;
+    if (e.target === document.activeElement || e.button !== 0) return
+    this.__isMouseDown = true
 
-    e.preventDefault();
+    e.preventDefault()
 
-    this.dragged = false;
-    this.startX = e.screenX;
-    this.startValue = this.state.value;
+    this.dragged = false
+    this.startX = e.screenX
+    this.startValue = this.state.value
 
-    window.addEventListener('mousemove', this.onMouseMove);
-    window.addEventListener('mouseup', this.onMouseUp);
+    window.addEventListener('mousemove', this.onMouseMove)
+    window.addEventListener('mouseup', this.onMouseUp)
   },
-  onMouseUp: function(e) {
+  onMouseUp: function (e) {
     if (this.__isMouseDown) {
-      e.preventDefault();
-      window.removeEventListener('mousemove', this.onMouseMove);
-      window.removeEventListener('mouseup', this.onMouseUp);
-      if (this.dragged) this.onBlur();
-      this.__isMouseDown = false;
+      e.preventDefault()
+      window.removeEventListener('mousemove', this.onMouseMove)
+      window.removeEventListener('mouseup', this.onMouseUp)
+      if (this.dragged) this.onBlur()
+      this.__isMouseDown = false
     }
   },
-  onDoubleClick: function(e) {
-    e.target.focus();
+  onDoubleClick: function (e) {
+    e.target.focus()
   },
-  onKeyDown: function(e) {
-    var value;
-    if (e.which == 38) {
+  onKeyDown: function (e) {
+    var value
+    if (e.which === 38) {
       // UP
-      e.preventDefault();
-      value = this.state.value + this.props.step;
-      this.setState({ value: value });
-      this.props.onInput(value);
-    } else if (e.which == 40) {
+      e.preventDefault()
+      value = this.state.value + this.props.step
+      this.setState({ value: value })
+      this.props.onInput(value)
+    } else if (e.which === 40) {
       // DOWN
-      e.preventDefault();
-      value = this.state.value - this.props.step;
-      this.setState({ value: value });
-      this.props.onInput(value);
-    } else if (e.which == 13) {
+      e.preventDefault()
+      value = this.state.value - this.props.step
+      this.setState({ value: value })
+      this.props.onInput(value)
+    } else if (e.which === 13) {
       // ENTER
-      this.onBlur(e);
-      e.target.blur();
+      this.onBlur(e)
+      e.target.blur()
     }
   },
-  render: function() {
-    /* jshint ignore:start */
+  render: function () {
     return (
       <div>
         <input
@@ -128,4 +127,4 @@ var TangleText = React.createClass({
   }
 });
 
-module.exports = TangleText;
+module.exports = TangleText
